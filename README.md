83b7a79748c82c4fe84d11b21ea8026ee18dc233
Francis S. Spencer <fsspencer@Franciss-MacBook-Pro.local>
2017-05-16 10:39:09 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# magento-export-tiny-database
-Script with the ability to generated a reduced database dump
+Welcome to StackEdit!
+===================
+
+
+Hey! I'm your first Markdown document in **StackEdit**[^stackedit]. Don't delete me, I'm very helpful! I can be recovered anyway in the **Utils** tab of the <i class="icon-cog"></i> **Settings** dialog.
+
+----------
+
+
+Install
+-------------
+
+Copy and paste the shell directory in your root project directory. Our shell directory contains a simple php script which generates a bash script in order to perform the dump files (one for schema and other for data).
+
+> **Requirements:**
+
+> - PHP should be accesible via command line using the command "php".
+> - Mysqldump command should be usable from command line.
+
+----------
+
+
+Options
+-------------
+
+By default our script will ignore all the data from log tables and report tables but also offers the ability to ignore orders or flat catalog tables by using the following arguments.
+
+    # Ignore orders 
+    php -f shell/generate_tiny_dump.php -- --ignore-orders
+    
+    # Ignore flat catalog tables
+    php -f shell/generate_tiny_dump.php -- --ignore-flat-catalog
+    
+    # Ignore only logs and reports
+    php -f shell/generate_tiny_dump.php
+
+After executing the php script you need to execute the resultant bash script:
+
+    sh shell/dump_data.sh
+    
+This will generate a compressed file on your project root with both sql scripts on it.
---
+++ shell/generate_tiny_dump.php
@@ -0,0 +1,181 @@
+<?php
+
+require_once 'abstract.php';
+ini_set('display_errors', 1);
+
+class Codealist_Shell_GenerateTinyDump extends Mage_Shell_Abstract
+{
+
+    /**
+     * Runs the process
+     */
+    public function run()
+    {
+        $db = $this->getDbInfo();
+        $fileNameSchema = $db['name'] . '-schema-' . date('j-m-y-h-i-s') . '.sql';
+        $fileNameData = $db['name'] . '-data-' . date('j-m-y-h-i-s') . '.sql';
+        
+        $dumpSchemaCmd = $this->getDumpCommand($fileNameSchema, true);
+        $dumpDataCmd = $this->getDumpCommand($fileNameData, false);
+
+        $fp = fopen(realpath(dirname(__FILE__)) . "/dump_data.sh", "w");
+        fputs($fp, "#!/bin/bash\n\n");
+        fputs($fp, $dumpSchemaCmd."\n\n");
+        fputs($fp, $dumpDataCmd."\n\n");
+     
+        $tarFile = $db['name'] . '-' . date('j-m-y-h-i-s') . '.gz';
+        fputs($fp, 'tar cfz ' . $tarFile . ' ' . $fileNameSchema . ' ' . $fileNameData . "\n\n");
+        fputs($fp, 'rm ' . $fileNameSchema . ' ' . $fileNameData);
+        fclose($fp);
+
+    }
+
+    public function getDumpCommand($fileName, $noData = false, $ignoreOrders = false)
+    {
+        $db = $this->getDbInfo();
+        $dumpSchema = 'mysqldump' . ' ';
+        if ($noData) {
+            $dumpSchema .= '--no-data' . ' ';
+        } else {
+            $dumpSchema .= $this->getIgnoreTables($ignoreOrders);
+        }
+        $dumpSchema .= '-u ' . $db['user'] . ' ';
+        $dumpSchema .= '--password="' . $db['pass'] . '" ';
+        $dumpSchema .= '-h ' . $db['host'] . ' ';
+        $dumpSchema .= '--port=' . $db['port'] . ' ';
+        $dumpSchema .= $db['name'] .' > ' . $fileName;
+      
+        return $dumpSchema;
+    }
+
+    public function getDbInfo()
+    {
+        $config  = Mage::getConfig()->getResourceConnectionConfig("default_setup");
+
+        $db = array(
+            'host' => (string)$config->host,
+            'user' => (string)$config->username,
+            'pass' => (string)$config->password,
+            'name' => (string)$config->dbname
+        );
+
+        $db['port'] = $this->getDatabasePort($db);
+      
+        return $db;
+    }
+
+    public function getDatabasePort($db)
+    {
+        $port = '3306';
+        if ($hostArr = explode($db['host'], ':')) {
+            $port = $hostArr[1];
+            $db['host'] = $hostArr[0];
+        }
+        return $port;
+    }
+
+    public function getIgnoreTables($ignoreOrders = false)
+    {
+        $tables = array(
+            'adminnotification_inbox',
+            'aw_core_logger',
+            'dataflow_batch_export',
+            'dataflow_batch_import',
+            'log_customer',
+            'log_quote',
+            'log_summary',
+            'log_summary_type',
+            'log_url',
+            'log_url_info',
+            'log_visitor',
+            'log_visitor_info',
+            'log_visitor_online',
+            'index_event',
+            'report_event',
+            'report_viewed_product_index',
+            'report_compared_product_index',
+            'catalog_compare_item',
+            'catalogindex_aggregation',
+            'catalogindex_aggregation_tag',
+            'catalogindex_aggregation_to_tag'
+        );
+
+        if ($ignoreOrders) {
+            $tables = array_merge($tables, array(
+                'sales_flat_creditmemo',
+                'sales_flat_creditmemo_comment',
+                'sales_flat_creditmemo_grid',
+                'sales_flat_creditmemo_item',
+                'sales_flat_invoice',
+                'sales_flat_invoice_comment',
+                'sales_flat_invoice_grid',
+                'sales_flat_invoice_item',
+                'sales_flat_order',
+                'sales_flat_order_address',
+                'sales_flat_order_grid',
+                'sales_flat_order_item',
+                'sales_flat_order_payment',
+                'sales_flat_order_shipping_rate',
+                'sales_flat_order_status_history',
+                'sales_flat_quote',
+                'sales_flat_quote_address',
+                'sales_flat_quote_address_item',
+                'sales_flat_quote_item',
+                'sales_flat_quote_item_option',
+                'sales_flat_quote_payment',
+                'sales_flat_quote_shipping_rate',
+                'sales_flat_shipment',
+                'sales_flat_shipment_comment',
+                'sales_flat_shipment_grid',
+                'sales_flat_shipment_item',
+                'sales_flat_shipment_track',
+                'sales_invoiced_aggregated',
+                'sales_invoiced_aggregated_order',
+                'sales_order_aggregated_created',
+                'sales_order_aggregated_updated',
+                'sales_order_status',
+                'sales_order_status_label',
+                'sales_order_status_state',
+                'sales_order_tax',
+                'sales_order_tax_item',
+                'sales_payment_transaction',
+                'sales_recurring_profile',
+                'sales_recurring_profile_order',
+                'sales_refunded_aggregated',
+                'sales_refunded_aggregated_order',
+                'sales_shipping_aggregated',
+                'sales_shipping_aggregated_order')
+            );
+        }
+      
+        $ignoreTables = ' ';
+        $db = $this->getDbInfo();
+        foreach($tables as $table) {
+            $ignoreTables .= '--ignore-table=' . $db['name'] . '.' . $db['pref'] . $table . ' ';
+        }
+      
+        return $ignoreTables;
+    }
+
+    /**
+     * Retrieve Usage Help Message
+     *
+     */
+    public function usageHelp()
+    {
+        return <<<USAGE
+Usage:  php -f generate.php -- [options]
+
+  --ignore-orders            Ignore Orders
+  --ignore-flat-catalog            Ignore Flat Catalog
+  help                                    This help
+
+
+USAGE;
+    }
+
+
+}
+
+$shell = new Codealist_Shell_GenerateTinyDump();
+$shell->run();
